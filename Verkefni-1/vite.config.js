efeefeee418c5fae0c92db8aff502ebf897508b0
Lavardur <antonben44@gmail.com>
2025-02-03 17:28:46 +0000

    ok

--- Verkefni-1/vite.config.js
+++ Verkefni-1/vite.config.js
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { configDefaults } from 'vitest/config';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  test: {
-    globals: true,
-    environment: 'jsdom', // Use 'jsdom' since we need DOM APIs like fetch
-    exclude: [...configDefaults.exclude],
-    setupFiles: './tests/setup.js', // Optional: A setup file to handle additional configs
+  // Configuration options
+  server: {
+    port: 3000,
   },
-})
+  build: {
+    outDir: 'dist',
+  },
+});
